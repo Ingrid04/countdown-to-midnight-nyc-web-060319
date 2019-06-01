bb29aff18710f3f4ef6cdbc468e278778552ca55
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 04:54:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
-
-def countdown
+number = 10
+countdown = 12
+def countdown  
+  loop do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    break if number <= 0
+  end
+  while countdown < 12
+   countdown -= 1
+    puts "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep
+  loop do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    break if number <= 0
+  end
+  while countdown < 12
+   countdown -= 1
+    puts "HAPPY NEW YEAR!"
+    sleep 1
+  end
+end
+
+  
